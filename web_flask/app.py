95a916302b9e6775facddea768178afa6839817a
khan2626 <israelkhan217@gmail.com>
2024-02-16 19:19:36 +0100

    added Node.js

--- web_flask/app.py
+++ web_flask/app.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 def home():
     """Displays the home page of 9jafoods"""
     return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
