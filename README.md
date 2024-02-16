0332a1e62b0beb34b45a7f9c8cdd14a7941d1d99
khan2626 <israelkhan217@gmail.com>
2024-02-16 11:51:02 +0100

    landing page

---
+++ README.md
@@ -0,0 +1,4 @@
+			9JAFOODS
+This is a portfolio project done by:
+Name: < Agi Ukanyohe Israel >
+Email: < israelkhan217@gmail.com >
---
+++ models/igbo_foods.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""It defines class Amenity"""
+from models.base_model import BaseModel
+
+
+class IgboFoods(BaseModel):
+    """It represent an Igbo food"""
+
+    name = ""
+    description_preparation = ""
